d10c44e927cb0e270fd655b5d5de42848cf14d13
YoinkOURCode <kevinkohwork@outlook.sg>
2025-10-09 12:43:01 +0800

    Fixing the dataset to fit the graph

--- utils/data_structures.py
+++ utils/data_structures.py
@@ -54,6 +54,97 @@ class RollingMeanArray:
             result[i] = sum(data[i - window + 1:i + 1]) / window
         return result
     
+class UpwardsDownwardsArray:
+    __slots__ = ("data","tolerance", "upwards", "downwards", "n")
+
+    def __init__(self, data: Iterable[float], tolerance: int = 3):
+        """
+        Efficient upward/downward run length calculator without NumPy or pandas.
+
+        Parameters
+        ----------
+        data : iterable of float
+            Numeric sequence (list or similar)
+        tolerance : int
+            Minimum length of consecutive runs to be considered significant
+            (default is 3)
+        """
+        data = list(data)
+        if len(data) < 2:
+            raise ValueError("data must contain at least two elements")
+
+        self.data: List[float] = data
+        self.n = len(data)
+
+        #Keeping track of upwards and downwards runs
+        self.upwards = 0
+        self.downwards = 0
+
+    def create_run_group(self) -> List[int]:
+        """
+        First computes consecutive differences in-place
+        Next, it will create a run group based on the sign of the differences.
+        If positive, it is an upward run; if negative, it is a downward run.
+        First element becomes NaN.
+
+        Parameters
+        ----------
+        values : iterable of floats
+
+        Returns
+        -------
+        list of integers
+        """
+        values = self.data
+
+        if not values:
+            return
+
+        prev = values[0]
+        values[0] = math.nan
+        for i in range(1, len(values)):
+            curr = values[i]
+            
+            values[i] = curr - prev
+            if values[i] > 0:
+                values[i] = 1  # Upward run
+                self.upwards += 1
+
+            elif values[i] < 0:
+                values[i] = -1  # Downward run
+                self.downwards += 1
+
+            else:
+                values[i] = 0  # No change
+            prev = curr
+
+        return values
+    
+    def create_run_group_naive(self) -> List[int]:
+        """
+        Naive way of computing consecutive differences in-place
+
+        Parameters
+        ----------
+        values : iterable of floats
+
+        Returns
+        -------
+        list of integers
+        """
+        values = self.data
+        n = len(values)
+
+        if n == 0:
+            return []
+
+        result = [math.nan] * n  # First element is NaN
+        for i in range(1, n):
+            result[i] = 1 if( values[i] - values[i - 1]) > 0 else -1 if (values[i] - values[i - 1]) < 0 else 0 # Upward run, Downward run, No change respectively
+
+        return result
+    
 
 
+   
 
